2f6fa55a321c468521411a8bbd6ea54a3b39a7c5
kris collins <kriscollinsmy@gmail.com>
2023-04-07 21:42:24 -0500

    Feat: Add preloader
    Preloader before threejs scene is loaded

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import "./App.css";
+import { Loader } from "./components";
+
+function App() {
+  return (
+    <>
+      <Loader />
+    </>
+  );
+}
+
+export default App;
