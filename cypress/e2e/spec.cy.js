98523e0f63d9cece499e9822203f6dbc0654c993
kris collins <kriscollinsmy@gmail.com>
2023-06-20 17:18:00 -0500

    [test] update cypress E2E test cases

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -5,20 +5,12 @@ describe("test what users first see when navigating to web page", () => {
     // make assertions on the web page using
     // an array of different viewports
     it(`Should display the office scene and animation on ${size} screen`, () => {
-      cy.visit("http://localhost:5173/");
+      if (Cypress._.isArray(size)) {
+        cy.viewport(size[0], size[1]);
+      } else {
+        cy.viewport(size);
+      }
 
-      //canvas
-      cy.get("canvas") // Replace with the appropriate selector for the Three.js canvas element
-        .should("exist")
-        .should("be.visible");
-
-      //hero text
-      cy.get("#hero-text").should("be.visible");
-
-      cy.get("#viewMyWorkButton").should("be.visible");
-    });
-
-    it(`Should allow users to click on the view my work button on the ${size} screen`, () => {
       cy.visit("http://localhost:5173/");
 
       //canvas
@@ -36,7 +28,7 @@ describe("test what users first see when navigating to web page", () => {
 
       cy.wait(3000);
 
-      cy.get("iframe").should('be.visible');
+      cy.get("iframe").should("be.visible");
     });
   });
 });
